588c0705f9e37b91c392a80ef4c4c766eb4b6cb7
AmeritechDF <creative@ameritechmedia.com>
2019-06-25 16:21:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+const API_URL = 'https://api.openbrewerydb.org/breweries?by_city=boston';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    breweries: []
+  }
+  componentDidMount() {
+    const url = `${API_URL}`;
+    axios.get(url).then(response => response.data)
+    .then((data) => {
+      this.setState({ breweries: data })
+      console.log(this.state.breweries)
+    })
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-8">
+          <h1>Boston Breweries</h1>
+          {this.state.breweries.map((brewery) => (
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{brewery.name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {brewery.website_url}
+              </h6>
+            </div>
+          </div>
+        ))}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
