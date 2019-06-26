aac49c7fd7c64239c7c63c2d137e8e23c61b7206
drewtjohnson <dtjohnson1995@gmail.com>
2019-06-26 18:03:36 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-const API_URL = 'https://api.openbrewerydb.org/breweries?by_city=boston';
+import React, { component } from 'react';
+import Breweries from './containers/breweries';
+import Info from './components/Info';
 
-class App extends Component {
-  state = {
-    breweries: []
-  }
-  componentDidMount() {
-    const url = `${API_URL}`;
-    axios.get(url).then(response => response.data)
-    .then((data) => {
-      this.setState({ breweries: data })
-      console.log(this.state.breweries)
-    })
-  }
+class App extends React.PureComponent {
   render() {
     return (
-      <div className="container">
-        <div className="col-xs-8">
-          <h1>Boston Breweries</h1>
-          {this.state.breweries.map((brewery) => (
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">{brewery.name}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">
-                {brewery.website_url}
-              </h6>
-            </div>
-          </div>
-        ))}
-        </div>
+      <div className="container mx-auto mt-8 mb-4">
+        <Breweries />
       </div>
     );
   }
 }
+
 export default App;
--- src/containers/breweryinfo.js
+++ src/containers/breweryinfo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 const API_URL = 'https://api.openbrewerydb.org/breweries?by_city=boston';
 
 class Breweries extends Component {
@@ -21,22 +20,21 @@ class Breweries extends Component {
         <div className="col-xs-8">
           <h1>Boston Breweries</h1>
           {this.state.breweries.map((brewery) => (
-        <Router>
-          <div className="card"><Link to={'/info/'}>
+          <div className="card">
             <div className="card-body">
               <h5 className="card-title">{brewery.name}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">
-                {brewery.website_url}
-              </h6>
+              <ul className="list-unstyled">
+                <li>{brewery.website_url}</li>
+                <li>{brewery.street}</li>
+                <li>{brewery.city}</li>
+                <li>{brewery.state}</li>
+                <li>{brewery.postal_code}</li>
+              </ul>
             </div>
-            </Link>
           </div>
-        </Router>
-
         ))}
         </div>
       </div>
-
     );
   }
 }
